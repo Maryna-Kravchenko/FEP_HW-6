9a65c891b2cc5e70f3fccf2e2f42d3c7b27b4877
Maryna Kravchenko <6302421@gmail.com>
2022-05-28 23:25:42 -0600

    corrected scripts.js

--- scripts.js
+++ scripts.js
@@ -2,18 +2,20 @@ for (let i = 0; i >= 0;) {
 
     let count = prompt('Text something');
 
-    if (count !== 'break' && count !== 'continue') {
-        i++;
-        alert(i);
-    }
-
-    if (count === 'continue') {
-        i++;
+    if (count === '') {
+        prompt('Text something');
         continue;
+    } else {
+        if (count !== 'break' && count !== 'continue') {
+            i++;
+            alert(i);
+        }
+        if (count === 'continue') {
+            i++;
+            continue;
+        }
+        if (count === 'break') {
+            break;
+        }
     }
-
-    if (count === 'break') {
-        break;
-    }
-
 }
